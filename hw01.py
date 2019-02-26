1a7339c9049adae1ded493be6729c736000eff18
kimomojam <44159736+kimomojam@users.noreply.github.com>
2019-02-26 19:50:55 +0800

    Add files via upload

---
+++ hw01.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 17:22:58 2019
+
+@author: kimomo
+"""
+
+print('你的系級、學號、姓名是?')
+print('系級: 107\n學號: 107061588\n姓名: 陳鑒佑\n')
+print('你的程式背景有哪些?')
+print('matlab, c\n')
+print('為什麼修這堂課?')
+print('因為想要知道python如何跟人文結合\n')
+print('在這堂課想學到什麼?')
+print('想學到python類別如何使用和靜態、動態爬蟲')
